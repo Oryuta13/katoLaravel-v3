43e3b43d568daed9f4e13853ddd36a2c965fc62f
Oryuta13 <ryuta1113abcdol@gmail.com>
2024-07-16 23:54:11 +0900

    add migration file

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/test', [TestController::class, 'test'])
+->name('test');
 
 Route::get('/', function () {
     return view('welcome');
